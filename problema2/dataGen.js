63f793772d08072f145ba14c6854f6e7a20bff08
Kaka <abd@cin.ufpe.br>
2017-03-23 03:07:28 -0300

    Random numbers generation. Color scale missing.

---
+++ problema2/dataGen.js
@@ -0,0 +1,11 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function getData(nsamples, min, max){
+	var dataset = [];
+
+	for (var i = 0; i < nsamples; i++) {
+		dataset.push([getRandomInt(min, max), getRandomInt(min, max), getRandomInt(min, max), getRandomInt(min, max)] );
+	}
+	return dataset
+}
--- problema2/problema2.js
+++ problema2/problema2.js
@@ -1,6 +1,6 @@
-var margin = {top: 20, right: 30, bottom: 20, left: 30};
-var width = 500- margin.left - margin.right;
-var height = 350 - margin.top - margin.bottom;
+var margin = {top: 20, right: 20, bottom: 20, left: 20};
+var width = 400 - margin.left - margin.right;
+var height = 400 - margin.top - margin.bottom;
 
 //Definindo margens e SVG
 var mySVG = d3.select("body").append("svg")
@@ -11,20 +11,26 @@ var mySVG = d3.select("body").append("svg")
 
 
 //Definindo escalas
-var xScale = d3.scaleLinear().domain([0,100]).range([0,width]);
-var yScale = d3.scaleLinear().domain([0,100]).range([height,0]);
+var xScale = d3.scaleLinear().domain([0,100]).range([0+margin.left,width - margin.right]);
+var yScale = d3.scaleLinear().domain([0,100]).range([height - margin.top,0]);
+//--------Raio maior que a margem!!!!! CORRIGIR---------
+var zScale = d3.scaleLinear().domain([0, 100]).range([0, 30])
 
-var dataset = [[50,95, "Brasil"],[5000,25, "Brasil"],[50,50, "EUA"],[30,20, "Argentina"],[10,64, "EUA"],[27,39, "UK"]];
+var N = getRandomInt(10, 50);
+var dataset = getData(N, 0, 100);
+//
+
+//var dataset = [[0,0, 40], [0, 20, 100], [20, 0, 100], [60,60, 0]]
 
  //Define axis
 var xAxisGroup = mySVG.append("g")
                       .attr("class","axis")
                       //.attr("transform", "translate(0,"+(height-margin.top)+")");
-                      .attr("transform", "translate(" + 20  + ","+(height-margin.top)+")");
+                      .attr("transform", "translate(" + 0  + ","+(height-margin.top)+")");
 
 var yAxisGroup = mySVG.append("g")
                       .attr("class","axis")
-                      .attr("transform", "translate("+(margin.left )+"," + -10 +")");
+                      .attr("transform", "translate("+(margin.left )+"," + 0 +")");
 
 	   
 mySVG
@@ -32,19 +38,19 @@ mySVG
 .data(dataset)
 .enter()
 .append("circle")
-.attr("r","5")
 .attr("cx",function(d){return xScale(d[0]);})
-.attr("cy",function(d){return yScale(d[1]);});
+.attr("cy",function(d){return yScale(d[1]);})
+.attr("r",function(d){return zScale(d[2]);});
 
-mySVG
-.selectAll("text")
-.data(dataset)
-.enter()
-.append("text")
-.attr("x",function(d){return xScale(d[0]);})
-.attr("y",function(d){return yScale(d[1]);})
-.attr("fill","red")
-.text(function(d){return d;});
+// mySVG
+// .selectAll("text")
+// .data(dataset)
+// .enter()
+// .append("text")
+// .attr("x",function(d){return xScale(d[0]);})
+// .attr("y",function(d){return yScale(d[1]);})
+// .attr("fill","red")
+// .text(function(d){return d;});
 
 
 //Eixos
@@ -54,6 +60,7 @@ var yAxis = d3.axisLeft(yScale).ticks(5).tickSize(0);
 xAxisGroup.call(xAxis);
 yAxisGroup.call(yAxis);
 
+//-------fazer os ticks mudarem com o numero de bolinhas------------
 //remove 0 from xAxis
 xAxisGroup.selectAll(".tick")
     .each(function (d, i) {
