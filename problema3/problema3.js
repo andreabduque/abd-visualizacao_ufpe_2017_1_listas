3debb9c615e95a7627d313fa496e5ecc39a9aeb8
andreabduque <abd@cin.ufpe.br>
2017-03-24 14:34:26 -0300

    Three lines added

--- problema3/problema3.js
+++ problema3/problema3.js
@@ -6,9 +6,19 @@ var dates = ["1-Jan-12", "1-Feb-12", "1-Mar-12", "1-Apr-12", "1-May-12", "1-Jun-
 
 var parseTime = d3.timeParse("%d-%b-%y");
 
+dataset_low = []
+for(var i = 0; i < 12; i++){
+    dataset_low.push([average_low[i] , dates[i]]);
+}
+
 dataset_avg = []
 for(var i = 0; i < 12; i++){
     dataset_avg.push([average[i] , dates[i]]);
+}
+
+dataset_high = []
+for(var i = 0; i < 12; i++){
+    dataset_high.push([average_high[i] , dates[i]]);
 }
 
 var margin = {top: 20, right: 20, bottom: 20, left: 20};
@@ -25,18 +35,43 @@ var mySVG = d3.select("#chart").append("svg")
 d3.select("#chart").attr("align","left");
 
 //min and max temperature
-var min_temp = Math.min.apply(Math, average_low);
-var max_temp = Math.max.apply(Math, average_high);
+//var min_temp = Math.min.apply(Math, average_low);
+//var max_temp = Math.max.apply(Math, average_high);
 xScale = d3.scaleTime().domain([parseTime("1-Jan-12"), parseTime("1-Dec-12")]).range([0+margin.left, width - margin.right]);
-yScale = d3.scaleLinear().domain([11.7,28.0]).range([height - margin.top,0 + margin.bottom]);
+yScale = d3.scaleLinear().domain([10,30]).range([height - margin.top,0 + margin.bottom]);
 
-
+//Avg line
 mySVG
-.append("path")
-.data([dataset_avg])
-.attr("class", "line")
-.attr("d", d3.line().curve(d3.curveBasis).x(function(d) {return xScale(parseTime(d[1])); }).y(function(d) {console.log(d[0]); return yScale(d[0]); })
-)
+  .append("path")
+  .data([dataset_avg])
+  .attr("class", "line")
+  .style("stroke", "Khaki")
+  .attr("d", d3.line()
+                .curve(d3.curveBasis)
+                .x(function(d) {return xScale(parseTime(d[1])); })
+                .y(function(d) {return yScale(d[0]); }));
+//Avg low line
+mySVG
+  .append("path")
+  .data([dataset_low])
+  .attr("class", "line")
+  .style("stroke", "steelblue")
+  .attr("d", d3.line()
+                .curve(d3.curveBasis)
+                .x(function(d) {return xScale(parseTime(d[1])); })
+                .y(function(d) {return yScale(d[0]); }));
+
+//Avg low line
+mySVG
+  .append("path")
+  .data([dataset_high])
+  .attr("class", "line")
+  .style("stroke", "LightCoral")
+  .attr("d", d3.line()
+                .curve(d3.curveBasis)
+                .x(function(d) {return xScale(parseTime(d[1])); })
+                .y(function(d) {return yScale(d[0]); }));
+
 
 var xAxisGroup = mySVG.append("g")
                       .attr("class","axis")
@@ -47,7 +82,7 @@ var yAxisGroup = mySVG.append("g")
                       .attr("class","axis")
                       .attr("transform", "translate("+(margin.left )+"," + 0 +")");
 
-var xAxis = d3.axisBottom(xScale).tickSize(height-margin.bottom - margin.top - 8).tickPadding(-height + 20).ticks(11).tickFormat(d3.timeFormat("%b"));
+var xAxis = d3.axisBottom(xScale).tickSize(0).tickPadding(-20).ticks(11).tickFormat(d3.timeFormat("%b"));
 var yAxis = d3.axisRight(yScale).tickSize(width-margin.left - margin.right).tickPadding(-width).ticks(8);
 
 xAxisGroup.call(xAxis);
