977bb80120682607d88b41af3f1668da4333ef82
andreabduque <abd@cin.ufpe.br>
2017-03-24 10:52:37 -0300

    Line curv and axis ok.

--- problema3/problema3.js
+++ problema3/problema3.js
@@ -2,12 +2,14 @@
 var average = [22.1, 	22.4, 	21.8,	19.7, 	17.4, 16.3, 15.8, 17.1, 17.9, 19, 20.2, 19.2];
 var average_high = [27.3, 28, 27.2, 25.1, 23, 21.8, 21.8, 23.3, 23.9, 24.8, 25.9, 26.3, 24.9];
 var average_low = [18.7, 18.8, 18.2, 16.3, 13.8, 12.4, 11.7, 12.8, 13.9, 15.3, 16.6, 17.7, 15.5];
-var dates = [];
-for(var i = 0; i < 12; i++){
-    dates.push(new Date(1990, i, 1));
-}
+var dates = ["1-Jan-12", "1-Feb-12", "1-Mar-12", "1-Apr-12", "1-May-12", "1-Jun-12", "1-Jul-12", "1-Aug-12", "1-Sep-12", "1-Oct-12", "1-Nov-12", "1-Dec-12"];
 
-dataset = [average,average_high,average_low]
+var parseTime = d3.timeParse("%d-%b-%y");
+
+dataset_avg = []
+for(var i = 0; i < 12; i++){
+    dataset_avg.push([average[i] , dates[i]]);
+}
 
 var margin = {top: 20, right: 20, bottom: 20, left: 20};
 var width = 600 - margin.left - margin.right;
@@ -25,8 +27,16 @@ d3.select("#chart").attr("align","left");
 //min and max temperature
 var min_temp = Math.min.apply(Math, average_low);
 var max_temp = Math.max.apply(Math, average_high);
-xScale = d3.scaleLinear().domain([new Date(1990, 0, 1), new Date(1990, 11, 1)]).range([0+margin.left, width - margin.right]);
-yScale = d3.scaleLinear().domain([min_temp,max_temp]).range([height - margin.top,0 + margin.bottom]);
+xScale = d3.scaleTime().domain([parseTime("1-Jan-12"), parseTime("1-Dec-12")]).range([0+margin.left, width - margin.right]);
+yScale = d3.scaleLinear().domain([11.7,28.0]).range([height - margin.top,0 + margin.bottom]);
+
+
+mySVG
+.append("path")
+.data([dataset_avg])
+.attr("class", "line")
+.attr("d", d3.line().curve(d3.curveBasis).x(function(d) {return xScale(parseTime(d[1])); }).y(function(d) {console.log(d[0]); return yScale(d[0]); })
+)
 
 var xAxisGroup = mySVG.append("g")
                       .attr("class","axis")
@@ -37,7 +47,7 @@ var yAxisGroup = mySVG.append("g")
                       .attr("class","axis")
                       .attr("transform", "translate("+(margin.left )+"," + 10 +")");
 
-var xAxis = d3.axisBottom(xScale).tickSize(0).tickValues(dates).tickFormat(d3.timeFormat("%b")).tickPadding(-15);
+var xAxis = d3.axisBottom(xScale).tickSize(0).ticks(11).tickFormat(d3.timeFormat("%b")).tickPadding(-15);
 var yAxis = d3.axisLeft(yScale).ticks(8).tickSize(0);
 
 xAxisGroup.call(xAxis);
