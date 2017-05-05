c94f2f05e90c3bb4c264a9cac05182e4b99c1397
andreabduque <abd@cin.ufpe.br>
2017-05-05 10:13:14 -0300

    jota

--- lista3/problema1/neigh.js
+++ lista3/problema1/neigh.js
@@ -2,52 +2,79 @@
 //normalized euclidean distance
 var attributes = ["sepal_length", "sepal_width", "petal_length", "petal_width"]
 
-function get_iris_range(){
+function get_iris_range() {
   var ranges = []
-  for(i = 0; i < attributes.length; i++){
+  for (i = 0; i < attributes.length; i++) {
     att = attributes[i]
-    min = d3.min(iris, function(d) { return d[att]; })
-    max = d3.max(iris, function(d) { return d[att]; })
+    min = d3.min(iris, function(d) {
+      return d[att];
+    })
+    max = d3.max(iris, function(d) {
+      return d[att];
+    })
     ranges.push([min, max])
   }
   return ranges
 }
 //normalized euclidean distance
-function euclid_distance_normal(item1, item2, ranges){
+function euclid_distance_normal(item1, item2, ranges) {
   sum_square = 0
-  for(i = 0; i < attributes.length; i++){
+  for (i = 0; i < attributes.length; i++) {
     att = attributes[i]
     diff = item1[att] - item2[att]
-    sum_square += Math.pow(diff/(ranges[i][1] - ranges[i][0]), 2)
+    sum_square += Math.pow(diff / (ranges[i][1] - ranges[i][0]), 2)
   }
 
   return Math.sqrt(sum_square)
 }
 //Get k nearest neighbors
- function kneighbors(data, ranges, k){
-   near_neigh = []
-   //for every item
-   for(i = 0; i < data.length; i++){
-     //get nearest neighbors
-      item = data[i]
-      neigh = []
-      for(j = 0; j < data.length; j++){
-          other_item = data[j]
-          val = euclid_distance_normal(item, other_item, ranges)
-          //Ignore himself
-          if(val != 0){
-            obj = {id: j, dist: val}
-            neigh.push(obj)
-          }
+function kneighbors(data, ranges, k, i, item) {
+  //for every item
+  neigh = []
+  for (j = 0; j < data.length; j++) {
+    other_item = data[j]
+    val = euclid_distance_normal(item, other_item, ranges);
+    //Ignore himself
+    if (val != 0) {
+      obj = {
+        source: i,
+        target: j,
+        value: val
+      };
+      neigh.push(obj);
+    }
+  }
+  //Sort by minimum distances
+  near_neigh = neigh.sort(function(a, b) {
+    return a.value - b.value;
+  }).slice(0, k);
+
+  return near_neigh;
+}
+
+ranges = get_iris_range()
+graph = []
+for (let i = 0; i < iris.length; i++) {
+  kneigh = kneighbors(iris, ranges, 3, i, iris[i]);
+  graph = graph.concat(kneigh);
+}
+console.log(graph.length);
+
+graph = graph.reduce(
+  (list, x) => {
+    let put = true;
+
+    for (let element of list) {
+      if (x.source === element.target && x.target === element.source) {
+        put = false;
+        break;
       }
+    }
 
-      //Sort by minimum distances
-      neigh.sort(function(a,b) {
-          return a.dist - b.dist;
-      });
+    if (put) {
+      list.push(x);
+    }
 
-      near_neigh.push(neigh.slice(0,k))
-      break
-   }
-   return near_neigh
- }
+    return list;
+  }
+, []);
